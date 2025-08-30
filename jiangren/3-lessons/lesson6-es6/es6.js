daa96ac5b5ac30f9b9231bb456629ed62cc095fc
Chris <chrisdavis@ChrisdeMacBook-Pro.local>
2025-08-30 18:27:39 +1000

    add es part 2

---
+++ jiangren/3-lessons/lesson6-es6/es6.js
@@ -0,0 +1,7 @@
+const arr1 = [1, 2];
+const arr2 = [3, 4];
+const arr3 = [...arr1, ...arr2]; // array
+const arr4 = arr1 + arr2; // string
+console.log(arr3);
+console.log(arr4);
+console.log(typeof arr4);
