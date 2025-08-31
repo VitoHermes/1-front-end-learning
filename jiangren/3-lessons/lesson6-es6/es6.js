bb90a64ba43880e014ceca5d1be472b26e088b34
Chris <chrisdavis@ChrisdeMacBook-Pro.local>
2025-08-31 21:20:55 +1000

    add es6.js

--- jiangren/3-lessons/lesson6-es6/es6.js
+++ jiangren/3-lessons/lesson6-es6/es6.js
@@ -1,6 +1,6 @@
 const arr1 = [1, 2];
 const arr2 = [3, 4];
-const arr3 = [...arr1, ...arr2]; // array
+const arr3 = [...arr1, ...arr2]; //
 const arr4 = arr1 + arr2; // string
 console.log(arr3);
 console.log(arr4);
